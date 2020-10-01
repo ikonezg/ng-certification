4d42bd11ce36442e32d68b3ba3af98a1805dde91
Marko Berger <marko.berger@gmail.com>
2020-10-01 23:57:31 +0200

    prettier config

--- src/app/weather-long/weather-long.component.ts
+++ src/app/weather-long/weather-long.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
-  selector: 'app-weather-long',
-  templateUrl: './weather-long.component.html',
-  styleUrls: ['./weather-long.component.scss']
+	selector: 'app-weather-long',
+	templateUrl: './weather-long.component.html',
+	styleUrls: ['./weather-long.component.scss']
 })
 export class WeatherLongComponent implements OnInit {
+	constructor(private location: Location) {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	onNavigateBack(): void {
+		this.location.back();
+	}
 }
