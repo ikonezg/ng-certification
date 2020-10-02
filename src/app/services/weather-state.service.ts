cfb062ad859dd23348a3ba1a7a824b8f8a6ce7ee
Marko Berger <marko.berger@gmail.com>
2020-10-03 00:33:15 +0200

    finished

--- src/app/services/weather-state.service.ts
+++ src/app/services/weather-state.service.ts
@@ -2,25 +2,52 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root'
 })
 export class WeatherStateService {
-  private zipcodes: string[] = [];
-  private zipSubject = new BehaviorSubject<string[]>([]);
-  zip$ = this.zipSubject.asObservable();
-  constructor() {}
+	private zipcodes: string[] = [];
+	private zipSubject = new BehaviorSubject<string[]>([]);
+	zip$ = this.zipSubject.asObservable();
+	constructor() {
+		this.loadZipcodesLS();
+	}
 
-  addZipcode(zipcode: string) {
-    this.zipcodes = [...this.zipcodes, zipcode];
-    this.zipSubject.next(this.zipcodes);
-  }
+	addZipcode(zipcode: string) {
+		this.zipcodes = this.zipcodes.includes(zipcode)
+			? [...this.zipcodes]
+			: [...this.zipcodes, zipcode];
+		localStorage.setItem('zipcodes', JSON.stringify(this.zipcodes));
+		this.zipSubject.next(this.zipcodes);
+	}
 
-  removeZipcode(zipcode: string) {
-    this.zipcodes = this.zipcodes.filter((z) => z !== zipcode);
-    this.zipSubject.next(this.zipcodes);
-  }
+	removeZipcode(zipcode: string) {
+		this.zipcodes = this.zipcodes.filter(z => z !== zipcode);
+		localStorage.setItem('zipcodes', JSON.stringify(this.zipcodes));
+		console.log(this.zipcodes);
 
-  getZipcodes() {
-    return this.zip$;
-  }
+		this.zipSubject.next(this.zipcodes);
+	}
+
+	getZipcodes() {
+		return this.zip$;
+	}
+	removeZipcodeLS(zipcode: string) {
+		const zip = localStorage.getItem('zipcodes')
+			? JSON.parse(localStorage.getItem('zipcodes'))
+			: null;
+		localStorage.setItem(
+			'zipcodes',
+			JSON.stringify(zip.filter(z => z !== zipcode))
+		);
+	}
+
+	loadZipcodesLS() {
+		const zip = localStorage.getItem('zipcodes')
+			? JSON.parse(localStorage.getItem('zipcodes'))
+			: null;
+		if (zip) {
+			this.zipcodes = [...zip];
+			this.zipSubject.next(this.zipcodes);
+		}
+	}
 }
--- src/app/weathe-now/weather-card/weather-card.component.ts
+++ src/app/weathe-now/weather-card/weather-card.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Weather } from 'src/app/models/weather';
+import { WeatherStateService } from 'src/app/services/weather-state.service';
 import { WeatherProviderService } from '../../services/weather-provider.service';
 @Component({
 	selector: 'app-weather-card',
@@ -14,12 +15,16 @@ export class WeatherCardComponent implements OnInit {
 
 	weather$: Observable<Weather>;
 	errorObject: any;
-	constructor(private weatherService: WeatherProviderService) {}
+	constructor(
+		private weatherService: WeatherProviderService,
+		private weatherState: WeatherStateService
+	) {}
 
 	ngOnInit(): void {
 		this.weather$ = this.weatherService.getWeatherNow(this.zipcode).pipe(
 			catchError(err => {
 				this.errorObject = err;
+				this.weatherState.removeZipcodeLS(this.zipcode);
 				return throwError(err);
 			})
 		);
